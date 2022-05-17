0b177c9d309e30947529a997de385b4d4888cd8f
Lance Hebert <LSUHEBERT@gmail.com>
2022-05-16 18:42:50 -0700

    Removed UUID package

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -10,7 +10,7 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-scroll";
-import uuid from 'react-uuid'
+
 
 
 const pages = ["home", "resume", "skills", "projects"];
